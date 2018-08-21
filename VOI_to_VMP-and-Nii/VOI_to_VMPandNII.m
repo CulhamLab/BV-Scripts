89b57afb72d019734c949e34a71a87696d133492
kmstubbs <kmstubbs@gmail.com>
2018-08-21 10:48:30 -0400

    script for voi to nii

---
+++ VOI_to_VMP-and-Nii/VOI_to_VMPandNII.m
@@ -0,0 +1,42 @@
+function VOI_to_VMPandNII
+
+[fn_in,fol] = uigetfile('*.voi','Select VOI(s)','voi(s)');
+filename = fn_in(1:find(fn_in=='.',1,'last')-1);
+fp_out = [fol filename '.vmp'];
+fp_out_nii = [fol filename '.nii'];
+
+voi = xff([fol fn_in]);
+
+vmp = xff('vmp');
+
+vmp.Resolution = voi.OriginalVMRResolutionX;
+%vmp.NativeResolutionFile = 0;
+vmp.FileVersion = 5;
+
+map_empty = zeros(length(vmp.XStart:vmp.XEnd),length(vmp.YStart:vmp.YEnd),length(vmp.ZStart:vmp.ZEnd));
+
+for v = 1:voi.NrOfVOIs
+    name = voi.VOI(v).Name;
+    voxels = voi.VOI(v).Voxels;
+    
+    voxels=(voxels*-1) + (256/2);
+    voxels=[voxels(:,2)-vmp.XStart voxels(:,3)-vmp.YStart voxels(:,1)-vmp.ZStart]+1;
+    
+    map = map_empty;
+    ind = sub2ind(size(map),voxels(:,1),voxels(:,2),voxels(:,3));
+    map(ind) = 1;
+    
+    vmp.Map(v).Name = name;
+    vmp.Map(v).LowerThreshold = 0;
+    vmp.Map(v).UpperThreshold = 1;
+    vmp.Map(v).ShowPositiveNegativeFlag = 1;
+    vmp.Map(v).VMPData = map;
+end
+
+vmp.NrOfMaps = length(vmp.Map);
+
+vmp.SaveAs(fp_out);
+vmp.ExportNifti(fp_out_nii);
+
+vmp = xff('vmp');
+voi.ClearObject;
