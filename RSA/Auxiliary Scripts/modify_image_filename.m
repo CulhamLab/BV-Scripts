23d935e8bcbc77dfabc667041eefb4a349d6434c
kmstubbs <kmstubbs@gmail.com>
2018-11-21 15:46:01 -0500

    RSA - improvement to image aux scripts

--- RSA/Auxiliary Scripts/modify_image_filename.m
+++
@@ -1,8 +0,0 @@
-function [image_name] = modify_image_filename(condition_name)
-image_name = condition_name;
-
-%make any changes needed to get image filename from predictor name here:
-
-% %Example: Carol 2018
-% image_name(find(image_name==' ',1,'first')) = '_';
-% image_name(find(image_name==' ',1,'first')) = '';
