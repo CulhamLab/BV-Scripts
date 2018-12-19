e5ae02eb3246b6a52b20558943657137d7a35765
kmstubbs <kmstubbs@gmail.com>
2018-12-19 15:57:14 -0500

    started script for inverse mds image file

---
+++ RSA/Auxiliary Scripts/create_imageData_file_for_inverse_MDS.m
@@ -0,0 +1,6 @@
+%imageData contains:
+%filename:  Nx1 cell array of predictor names
+%image:     Nx1 cell array of images (uint8)
+%alpha:     optional, Nx1 cell array of image alpha values (0 to 1 or binary)
+function create_imageData_file_for_inverse_MDS
+
