edb061a99bf93531b15ff1ac242f43683b80785c
Kevin Stubbs <kmstubbs@gmail.com>
2021-10-27 12:21:06 -0400

    support per-subject VOI

--- RSA/ROI_STEP06_extractROI.m
+++ RSA/ROI_STEP06_extractROI.m
@@ -45,12 +45,16 @@ else
         p.VOI_FILE = {p.VOI_FILE};
     end
     
-    num_voi = length(p.VOI_FILE);
+    if size(p.VOI_FILE,1) == 1
+        p.VOI_FILE = repmat(p.VOI_FILE, [p.NUMBER_OF_PARTICIPANTS 1]);
+    end
+    num_voi = size(p.VOI_FILE,2);
+    
     for i = 1:num_voi
-        voi_path = p.VOI_FILE{i};
-        fprintf('Processing VOI file %d of %d (%s) ...\n', i, num_voi, voi_path);
+        voi_paths = p.VOI_FILE(:,i);
         
-        [voi_data(i),d] = process_voi(voi_path, p, readFol, do_all_split, selected);
+        fprintf('Processing VOI file %d of %d\n%s', i, num_voi, sprintf('\t%s\n',voi_paths{:}));
+        [voi_data(i),d] = process_voi(voi_paths, p, readFol, do_all_split, selected);
         if i==1
             runtime = d;
         end
@@ -76,7 +80,7 @@ for i = 2:length(voi_data)
         data.RSM_nonsplit(:,:,:,count_voi) = voi_data(i).data.RSM_nonsplit(:,:,:,j);
         data.VOINames{count_voi} = voi_data(i).data.VOINames{j};
         data.VOINames_short{count_voi} = voi_data(i).data.VOINames_short{j};
-        data.VOInumVox(count_voi) = voi_data(i).data.VOInumVox(j);
+        data.VOInumVox(:,count_voi) = voi_data(i).data.VOInumVox(:,j);
     end
 end
 
@@ -88,25 +92,29 @@ runtime.Step6 = p.RUNTIME;
 save([saveFol 'VOI_RSMs'],'data','vtcRes','runtime','do_all_split')
 disp Done.
 
-function [voi_data,runtime] = process_voi(voi_filepath, p, readFol, do_all_split, selected)
+function [voi_data,runtime] = process_voi(voi_filepaths, p, readFol, do_all_split, selected)
 
-%load VOI file
-if isnan(voi_filepath)
-    disp('Select a VOI file to use. This file should contain all ROIs.')
-    [fn_in,fp_in] = uigetfile('*.voi','VOI','INPUT','MultiSelect','off');
-    disp('Loading VOI...');
-    voi = xff([fp_in fn_in]);
-else
-    disp('Loading VOI...');
-    voi = xff(voi_filepath);
+%select
+if ~iscell(voi_filepaths)
+    if isnan(voi_filepaths)
+        disp('Select a VOI file to use. This file will be used for all participants.')
+        [fn_in,fp_in] = uigetfile('*.voi','VOI','INPUT','MultiSelect','off');
+        voi_filepaths = repmat({[fp_in fn_in]}, [p.NUMBER_OF_PARTICIPANTS 1]);
+    else
+        error('Unexpected input voi_filepaths')
+    end
 end
 
+%load
+disp('Loading first VOI as reference...');
+voi_ref = xff(voi_filepaths{1});
+
 %init data struct
-data.RSM_split = nan(p.NUMBER_OF_CONDITIONS,p.NUMBER_OF_CONDITIONS,p.NUMBER_OF_PARTICIPANTS,voi.NrOfVOIs);
-data.RSM_nonsplit = nan(p.NUMBER_OF_CONDITIONS,p.NUMBER_OF_CONDITIONS,p.NUMBER_OF_PARTICIPANTS,voi.NrOfVOIs);
-data.VOINames = cell(1,voi.NrOfVOIs);
-data.VOINames_short = cell(1,voi.NrOfVOIs);
-data.VOInumVox = zeros(1,voi.NrOfVOIs);
+data.RSM_split = nan(p.NUMBER_OF_CONDITIONS,p.NUMBER_OF_CONDITIONS,p.NUMBER_OF_PARTICIPANTS,voi_ref.NrOfVOIs);
+data.RSM_nonsplit = nan(p.NUMBER_OF_CONDITIONS,p.NUMBER_OF_CONDITIONS,p.NUMBER_OF_PARTICIPANTS,voi_ref.NrOfVOIs);
+data.VOINames = cell(1,voi_ref.NrOfVOIs);
+data.VOINames_short = cell(1,voi_ref.NrOfVOIs);
+data.VOInumVox = zeros(1,voi_ref.NrOfVOIs);
 
 %count
 number_splits = size(selected,1);
@@ -116,62 +124,65 @@ disp('Processing participant data...')
 for par = 1:p.NUMBER_OF_PARTICIPANTS
     fprintf('Processing participant %d of %d: %s\n', par, p.NUMBER_OF_PARTICIPANTS, p.FILELIST_PAR_ID{par});
     
-    %load
-    fprintf('\tLoading...\n');
+    %load voi
+    fprintf('\tLoading VOI: %s\n', voi_filepaths{par});
+    voi = xff(voi_filepaths{par});
+    
+    %load data
+    fprintf('\tLoading processed betas...\n');
     subdata = load(sprintf('%sstep3_organize3D_%s',readFol,p.FILELIST_PAR_ID{par}));
     
     %use settings of first file
     fprintf('\tChecking settings...\n');
-    if par == 1
+    if ~exist('vtcRes', 'var')
         %func res
         vtcRes = subdata.vtcRes;
-        
-        %runtime data
-        if isfield(subdata, 'runtime')
-            runtime = subdata.runtime;
-        else
-            runtime = struct;
-        end
-        
-        %prep ROIs
-        fprintf('\tPreparing ROIs based on settings...\n');
-        for vid = 1:voi.NrOfVOIs %for each voi...
-           %name of voi
-           data.VOINames{vid} = voi.VOI(vid).Name;
-           data.VOINames_short{vid} = data.VOINames{vid};
-
-           %convert TAL VOI coords to save coords
-           vox_TAL = voi.VOI(vid).Voxels;
-           returnPath = pwd;
-            try
-                cd('Required Methods')
-                [X,Y,Z] = SAVE_SYSTEM_COORD_CONVERSION( vox_TAL , vtcRes );
-                cd ..
-            catch e
-                cd(returnPath)
-                rethrow(e)
-            end
-           xyz = [X,Y,Z];
-
-           %remove anything outside data
-           voi_xyz{vid} = xyz(find(~isnan(sum(xyz,2))),:);
-           
-           sz = size(subdata.betas_3D_even);
-           sz = sz(1:3);
-           inval = any( (voi_xyz{vid} < 1) | (voi_xyz{vid} > sz) , 2);
-            if any(inval)
-                num_keep = sum(~inval);
-                num_toss = sum(inval);
-                fprintf('Discarding %d voxels outside of bounding box (kept %d voxels)\n', num_toss, num_keep);
-                voi_xyz{vid}(inval,:) = [];
-            end
-
-           %param
-           data.VOInumVox(vid) = size(voi_xyz{vid},1);
-        end
-        
     elseif subdata.vtcRes ~= vtcRes
         error('mismatch in vtcRes')
+    end
+    
+    %get first data runtime
+    if ~exist('runtime', 'var') && isfield(subdata, 'runtime')
+        runtime = subdata.runtime;
+    end
+    
+    %prep ROIs
+    fprintf('\tPreparing ROIs based on settings...\n');
+    for vid = 1:voi.NrOfVOIs %for each voi...
+       %name of voi
+       if isempty(data.VOINames{vid})
+            data.VOINames{vid} = voi.VOI(vid).Name;
+            data.VOINames_short{vid} = data.VOINames{vid};
+       end
+
+       %convert TAL VOI coords to save coords
+       vox_TAL = voi.VOI(vid).Voxels;
+       returnPath = pwd;
+        try
+            cd('Required Methods')
+            [X,Y,Z] = SAVE_SYSTEM_COORD_CONVERSION( vox_TAL , vtcRes );
+            cd ..
+        catch e
+            cd(returnPath)
+            rethrow(e)
+        end
+       xyz = [X,Y,Z];
+
+       %remove anything outside data
+       voi_xyz{vid,par} = xyz(find(~isnan(sum(xyz,2))),:);
+
+       sz = size(subdata.betas_3D_even);
+       sz = sz(1:3);
+       inval = any( (voi_xyz{vid,par} < 1) | (voi_xyz{vid,par} > sz) , 2);
+        if any(inval)
+            num_keep = sum(~inval);
+            num_toss = sum(inval);
+            fprintf('Discarding %d voxels outside of bounding box (kept %d voxels)\n', num_toss, num_keep);
+            voi_xyz{vid,par}(inval,:) = [];
+        end
+
+       %param
+       data.VOInumVox(par,vid) = size(voi_xyz{vid,par},1);
     end
         
     %check do_all_split
@@ -185,12 +196,12 @@ for par = 1:p.NUMBER_OF_PARTICIPANTS
         fprintf('\t\tProcessing ROI %d of %d: %s\n', vid, voi.NrOfVOIs, data.VOINames{vid});
         
         %preinit [vox# cond# odd/even/all]
-        betas = nan(data.VOInumVox(vid),p.NUMBER_OF_CONDITIONS,3);
+        betas = nan(data.VOInumVox(par,vid),p.NUMBER_OF_CONDITIONS,3);
         
         %for each cond, gather all betas
         for cond = 1:p.NUMBER_OF_CONDITIONS
             %all three matrices have the same shape so we only need one index
-            ind_vox_thiscond = sub2ind(size(subdata.betas_3D_even),voi_xyz{vid}(:,1),voi_xyz{vid}(:,2),voi_xyz{vid}(:,3),repmat(cond,[data.VOInumVox(vid) 1]));
+            ind_vox_thiscond = sub2ind(size(subdata.betas_3D_even),voi_xyz{vid,par}(:,1),voi_xyz{vid,par}(:,2),voi_xyz{vid,par}(:,3),repmat(cond,[data.VOInumVox(par,vid) 1]));
             
             betas(:,cond,1) = subdata.betas_3D_even(ind_vox_thiscond); %even
             betas(:,cond,2) = subdata.betas_3D_odd(ind_vox_thiscond); %odd
@@ -244,17 +255,17 @@ for par = 1:p.NUMBER_OF_PARTICIPANTS
             
             for split = 1:number_splits
                 %init
-                betas_group1 = nan(data.VOInumVox(vid), p.NUMBER_OF_CONDITIONS);
-                betas_group2 = nan(data.VOInumVox(vid), p.NUMBER_OF_CONDITIONS);
+                betas_group1 = nan(data.VOInumVox(par,vid), p.NUMBER_OF_CONDITIONS);
+                betas_group2 = nan(data.VOInumVox(par,vid), p.NUMBER_OF_CONDITIONS);
                 
                 %process betas_3D_runs(x,y,z,run,cond)
-                for v = 1:data.VOInumVox(vid)
+                for v = 1:data.VOInumVox(par,vid)
                     %group 1
-                    betas_run = squeeze(subdata.betas_3D_runs(voi_xyz{vid}(v,1), voi_xyz{vid}(v,2), voi_xyz{vid}(v,3), selected(split,:), :));
+                    betas_run = squeeze(subdata.betas_3D_runs(voi_xyz{vid,par}(v,1), voi_xyz{vid,par}(v,2), voi_xyz{vid,par}(v,3), selected(split,:), :));
                     betas_group1(v,:) = nanmean(betas_run, 1) - nanmean(betas_run(:));
                     
                     %group 2
-                    betas_run = squeeze(subdata.betas_3D_runs(voi_xyz{vid}(v,1), voi_xyz{vid}(v,2), voi_xyz{vid}(v,3), ~selected(split,:), :));
+                    betas_run = squeeze(subdata.betas_3D_runs(voi_xyz{vid,par}(v,1), voi_xyz{vid,par}(v,2), voi_xyz{vid,par}(v,3), ~selected(split,:), :));
                     betas_group2(v,:) = nanmean(betas_run, 1) - nanmean(betas_run(:));
                 end
                 
@@ -287,8 +298,17 @@ for par = 1:p.NUMBER_OF_PARTICIPANTS
         
         %average odd/even and even/odd to create symmetrical RSM
         data.RSM_split(:,:,par,vid) = arrayfun(@(x,y) nanmean([x y]), data.RSM_split(:,:,par,vid), data.RSM_split(:,:,par,vid)');
-   end
+    end
+   
+    voi.ClearObject;
+    clear voi;
 end
 
 voi_data.data = data;
 voi_data.vtcRes = vtcRes;
+
+voi_ref.ClearObject;
+
+if ~exist('runtime', 'var')
+    runtime = struct;
+end
